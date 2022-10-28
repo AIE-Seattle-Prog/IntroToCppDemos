3b2f22963d0742fa9fb0a136919fb03f6f37be87
Terry Nguyen <terry@terrehbyte.com>
2022-10-28 15:00:30 -0700

    Add CTOR and DTOR samples

---
+++ CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿int input = 5;
+
+if(input > 3)
+{
+    // input x 3 if big enough
+    int val = input * 3;
+
+    Console.WriteLine(input);
+    Console.WriteLine(val);
+}
+
+//Console.WriteLine(val);
